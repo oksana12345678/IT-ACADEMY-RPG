b3229a529fc635c28b43c2b5c73b29d66e77a97b
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2025-03-28 22:15:46 +0100

    update team list title

--- src/modules/footer/components/TeamList/TeamList.jsx
+++ src/modules/footer/components/TeamList/TeamList.jsx
@@ -16,7 +16,8 @@ const getColorClass = (position) => {
     position.toLowerCase().includes('дизайн')
   )
     return colorMapping.design;
-  if (position.toLowerCase().includes('менеджер')) return colorMapping.manager;
+  if (position.toLowerCase().includes('менеджер' || 'Manager'))
+    return colorMapping.manager;
   if (position.toLowerCase().includes('qa')) return colorMapping.qa;
   return 'bg-gray-300';
 };
@@ -27,11 +28,11 @@ const TeamList = () => {
   return (
     <div className="flex w-full h-full xl:py-3 xl:px-15 md:py-8 md:px-8 py-3 justify-center items-center xl:items-start">
       <div className="flex flex-col h-full  w-fit  xl:items-start">
-        <h2 className="text-[#FF9900] font-semibold xl:text-3xl md:text-2xl">
+        <h2 className="text-[#FF9900] font-semibold xl:text-3xl md:text-2xl xl:pb-8 md:6 pb-4">
           {t('title')}
         </h2>
         <div className="flex flex-col gap-4 xl:flex-row ">
-          <ul className="grid gap-y-[24px] gap-x-[40px] grid-cols-1 md:grid-cols-2">
+          <ul className="grid xl:gap-y-6 xl:gap-x-10 md:gap-y-5 md:gap-x-6 gap-y-3 grid-cols-1 md:grid-cols-2">
             {t('list', { returnObjects: true }).map((member, index) => (
               <li
                 className="flex flex-col md:gap-2 gap-1.5 xl:w-[314px] md:w-[270px]"
